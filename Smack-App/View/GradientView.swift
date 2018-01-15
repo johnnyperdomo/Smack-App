2c76234e54f0c2ae2ba927e5e67ce4f03e8fc2ac
Johnny Perdomo <johnnyperdomo@iMac.local>
2018-01-14 23:01:45 -0500

    updated channelVC, added Gradient View

---
+++ Smack-App/View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  Smack-App
+//
+//  Created by Johnny Perdomo on 1/14/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable //lets it know, this file needs to render inside the story board
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1) { // @IBInspectable, lets you change the properties in the app
+        didSet { //when we set the new uicolor, didset
+            self.setNeedsLayout() //invalidates current layout, and updates it to whatever you chose
+        }
+    }
+  
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) { // @IBInspectable, lets you change the properties in the app
+        didSet { //when we set the new uicolor, didset
+            self.setNeedsLayout() //invalidates current layout, and updates it to whatever you chose
+        }
+    }
+    
+    //when we update the views using the setNeedsLayout(), this function is called...//this is what happens after the update
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer() //gradients need three things...colors, start & end point, how big its gonna be
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor] //these are the colors youre able to edit
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0) //(0,0) starts top left using the ios coordinate system.... we know that the gradient goes from top to bottm because it starts at (0,0) and ends at (1,1)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1) // (1,1) is the bottom right corner of screen using the ios coordinate system
+        gradientLayer.frame = self.bounds //the size of the gradient layer in the view
+        self.layer.insertSublayer(gradientLayer, at: 0) //this is to add the layer to the UIView's layer
+    }
+    
+}
