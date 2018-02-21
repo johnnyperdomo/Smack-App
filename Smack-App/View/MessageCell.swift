bfa955769a5ae5367e9ea4123d926b9c03944fb5
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-02-21 17:16:29 -0500

    messages now have a timestamp

--- Smack-App/View/MessageCell.swift
+++ Smack-App/View/MessageCell.swift
@@ -30,6 +30,22 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        // 2017-21-13T21:49:25.590Z //we wanna remove the milliseconds that apple gives
+        guard var isoDate = message.timeStamp else { return } //unwrap our date
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5) //start at the end, then remove the last 5
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter() //this is ios date formatter built in by apple
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z")) // now we have an actual data that looks like 2017-21-13T21:49:25Z
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a" //month, day, hour, minute, am/pm
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate) //converting from a date to a string, makes it more readable to the user
+            timeStampLbl.text = finalDate //send the timestamplbl
+        }
     }
 
 }
